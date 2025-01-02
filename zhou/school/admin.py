e4a22cac1494198f805ca64bae7b4024b20d8001
zimthug <tmlangeni@gmail.com>
2025-01-02 10:15:27 +0200

    Made a few changes to the admin for the class

--- zhou/school/admin.py
+++ zhou/school/admin.py
@@ -4,5 +4,21 @@ from .models import Clazz, Term, TermClazz
 
 
 admin.site.register(Clazz)
-admin.site.register(Term)
-admin.site.register(TermClazz)
+
+
+@admin.register(Term)
+class TermAdmin(admin.ModelAdmin):
+    list_display = ('year', 'name', 'start_date', 'end_date', 'is_active', 'is_enrollment_active')
+    list_filter = ('year', 'name')
+
+
+@admin.register(TermClazz)
+class TermClazzAdmin(admin.ModelAdmin):
+    list_display = ('term', 'clazz', 'start_on', 'end_on')
+    list_filter = ('term', 'clazz')
+
+    def start_on(self, obj):
+        return obj.start_date
+
+    def end_on(self, obj):
+        return obj.end_date
